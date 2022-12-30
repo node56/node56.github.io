741302b48553f4654743d3a0011458a6935b75b1
node56 <github@o56o.com>
2022-12-29 22:41:37 -0800

    factor out grid making

--- script.js
+++ script.js
@@ -72,6 +72,37 @@ function playMatch(player1, player2) {
     return player2;
   }
 }
+function getGrid(dir, grid) {
+    // Create a new grid to store the updated state of each cell
+  let rockTotal = 0;
+  let paperTotal = 0;
+  let scissorsTotal = 0;
+  
+  let newGrid = [];
+  for (let i = 0; i < GRID_HEIGHT; i++) {
+    let row = [];
+    for (let j = 0; j < GRID_WIDTH; j++) {
+      row.push(0);
+    }
+    newGrid.push(row);
+  }
+
+  // Iterate over each cell in the grid
+  for (let i = 0; i < GRID_HEIGHT; i++) {
+    for (let j = 0; j < GRID_WIDTH; j++) {
+      let cellMove = grid[i][j];
+      if (dir % 4 === 0) { newGrid[i][j] = playMatch(cellMove, grid[(i + GRID_WIDTH -1) % GRID_WIDTH][j]); }
+      if (dir % 4 === 1) { newGrid[i][j] = playMatch(cellMove, grid[i][(j + GRID_HEIGHT -1) % GRID_HEIGHT]; }
+      if (dir % 4 === 2) { newGrid[i][j] = playMatch(cellMove, grid[(i + 1) % GRID_WIDTH][j]); }
+      if (dir % 4 === 3) { newGrid[i][j] = playMatch(cellMove, grid[i][(j + 1) % GRID_HEIGHT]); }
+      let c = newGrid[i][j];
+      if (c === "R") rockTotal++;
+      if (c === "P") paperTotal++;
+      if (c === "S") scissorsTotal++;
+    }
+  }
+  return [newGrid, rockTotal, paperTotal, scissorsTotal];
+}
 
 // Create the grid elements and add them to the webpage
 const gridContainer = document.getElementById("grid-container");
@@ -98,40 +129,7 @@ for (let i = 0; i < GRID_HEIGHT; i++) {
 
 // Play the game
 setInterval(() => {
-  // Create a new grid to store the updated state of each cell
-  let rockTotal = 0;
-  let paperTotal = 0;
-  let scissorsTotal = 0;
-  
-  let newGrid = [];
-  for (let i = 0; i < GRID_HEIGHT; i++) {
-    let row = [];
-    for (let j = 0; j < GRID_WIDTH; j++) {
-      row.push(0);
-    }
-    newGrid.push(row);
-  }
-
-  // Iterate over each cell in the grid
-  for (let i = 0; i < GRID_HEIGHT; i++) {
-    for (let j = 0; j < GRID_WIDTH; j++) {
-      // Get the moves of the current cell and its neighbors
-      let cellMove = grid[i][j];
-      let topMove = grid[(i + GRID_WIDTH -1) % GRID_WIDTH][j];
-      let bottomMove = grid[(i + 1) % GRID_WIDTH][j];
-      let leftMove = grid[i][(j + GRID_HEIGHT -1) % GRID_HEIGHT];
-      let rightMove = grid[i][(j + 1) % GRID_HEIGHT];
-      dir = Math.floor(Math.random() * 4)
-      if (dir % 4 === 0) { newGrid[i][j] = playMatch(cellMove, topMove); }
-      if (dir % 4 === 1) { newGrid[i][j] = playMatch(cellMove, leftMove); }
-      if (dir % 4 === 2) { newGrid[i][j] = playMatch(cellMove, bottomMove); }
-      if (dir % 4 === 3) { newGrid[i][j] = playMatch(cellMove, rightMove); }
-      let c = newGrid[i][j];
-      if (c === "R") rockTotal++;
-      if (c === "P") paperTotal++;
-      if (c === "S") scissorsTotal++;
-    }
-  }
+  const  [newGrid, rockTotal, paperTotal, scissorsTotal] = getGrid(Math.floor(Math.random() * 4), grid);
   round++;
  
   // Update the grid with the new state of each cell
